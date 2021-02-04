2a5f7b502a9d9e5de5444704fc4906518912d3ad
Gui-Yue <yuemeng.gui@gmail.com>
2021-02-04 12:10:03 +0800

    ok
    ok

--- django backup/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib.auth.views import LoginView
-from . import views
-urlpatterns = [
-    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
-    url(r'^logout/$', views.logout_views, name='logout'),
-    url(r'^register/$', views.register, name='register')
-]
